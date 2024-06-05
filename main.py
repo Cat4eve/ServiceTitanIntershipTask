dd17c995605b403ddb51f3a163942851fcc00e7d
Cat4eve <71460804+Cat4eve@users.noreply.github.com>
2024-06-05 17:54:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from data_extractor import DataExtractor
 
 if __name__ == '__main__':
     D = DataExtractor()
-    arr = D._load_dataset('invoices_new.pkl')
+    D._load_dataset('invoices_new.pkl')
     D._load_expired_list('expired_invoices.txt')
     D._transform_to_flat_data()
